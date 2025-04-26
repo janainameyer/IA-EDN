a447d0aa0b3ed8446118ff1ece27f3d066742e36
janainameyer <contatrabalhoprimeira@gmail.com>
2025-04-25 22:06:06 -0300

    Terceira aula de IA na EDN

--- algoritmos.py
+++ algoritmos.py
@@ -1,4 +1,5 @@
-# Algoritmo de busca
+# Algoritmo de busca - linear
+
 def busca(lista, nome_procurado):
     for i in range(len(lista)):
         if lista[i] == nome_procurado:
@@ -13,3 +14,29 @@ if posicao != -1:
     print(f"{nome_procurado} está na posição: {posicao}")
 else:
     print("O nome não está na lista.")
+
+# Algoritmo de busca binária
+def binaria(lista, nome_procurado):
+    inicio = 0
+    fim = len(lista) - 1
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        if lista[meio] == nome_procurado:
+            return meio
+        elif lista[meio] < nome_procurado:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+    return -1 
+
+numeros = [1, 3, 5, 7, 9, 11, 13, 15]
+posicao = binaria(numeros, 7)
+
+if posicao != -1:
+    print("Número encontrado na posição:", posicao)
+else:
+    print("Número não encontrado.")
+
+
+    
+
